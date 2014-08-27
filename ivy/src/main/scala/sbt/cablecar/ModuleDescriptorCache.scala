045814290a1aec809797cd9a7e3be042ac07f8ac
Eugene Yokota <eed3si9n@gmail.com>
2014-08-27 06:15:46 -0400

    cache when modules are not found

--- ivy/src/main/scala/sbt/cablecar/ModuleDescriptorCache.scala
+++ ivy/src/main/scala/sbt/cablecar/ModuleDescriptorCache.scala
@@ -10,22 +10,25 @@ import collection.mutable
 
 private[sbt] final class ModuleDescriptorCache {
   import ModuleDescriptorCache._
-  val mds: mutable.Map[(String, String, String), ModuleDescriptorCacheEntry] = mutable.Map()
+  // cache the fact that the module was unavailable too
+  val mds: mutable.Map[(String, String, String), Option[ModuleDescriptorCacheEntry]] = mutable.Map()
   def loadModuleDescriptor(ivyData: IvyResolveData, resolver: DependencyResolver, dd: DependencyDescriptor): Option[ModuleDescriptorCacheEntry] =
     {
       val mrid = dd.getDependencyRevisionId
       val key = (mrid.getOrganisation, mrid.getName, mrid.getRevision) // IvyRetrieve.toModuleID(requestedRevisionId).copy(configurations = None)
-      mds.get(key) orElse {
-        val moduleOpt = Option(resolver.getDependency(dd, ivyData))
-        moduleOpt map { module =>
-          val md = module.getDescriptor
-          val resolver = module.getResolver
-          val artifactResolver = module.getArtifactResolver
-          resolver.getRepositoryCacheManager.saveResolvers(md, resolver.getName, artifactResolver.getName)
-          val x = ModuleDescriptorCacheEntry(md, resolver, artifactResolver)
+      mds.get(key) match {
+        case Some(opt) => opt
+        case _ =>
+          val moduleOpt = Option(resolver.getDependency(dd, ivyData))
+          val x = moduleOpt map { module =>
+            val md = module.getDescriptor
+            val resolver = module.getResolver
+            val artifactResolver = module.getArtifactResolver
+            resolver.getRepositoryCacheManager.saveResolvers(md, resolver.getName, artifactResolver.getName)
+            ModuleDescriptorCacheEntry(md, resolver, artifactResolver)
+          }
           mds += ((key, x))
           x
-        }
       }
     }
 
